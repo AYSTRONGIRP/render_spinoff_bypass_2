2092e58054807118196620738f1dabd6725939e1
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-05-30 00:15:35 +0530

    t

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const link = "https://render-spinoff-bypass-1.onrender.com"
-console.log("starting from render 1");
+console.log("starting from render 2");
 setInterval(() => {
     fetch(link).then(() =>{console.log(link);});
-}, 1000); //every 10 minutes
+}, 1000); //every 1 minutes
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from render 2!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
